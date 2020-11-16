dc29a7b167ef634063ae3b8f776417d8ddf0ba12
Uriel Aispuro <urielalexis64@gmail.com>
2020-11-15 23:33:50 -0700

    getTracks function added

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -7,14 +7,29 @@ import { Injectable } from '@angular/core';
 export class SpotifyService {
   constructor(private _http: HttpClient) {}
 
+  token =
+    'Bearer BQCNDQhzoEHBJ3gW_46ueKr6gIUh_c4AjIQaAOWkwFeOXm4uGUJXWjScem3tTIdFui-A4eGf-QHt5ehSdiw';
+
   getNewReleases() {
     const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQA-tecZqzHhLBQlUOv6SVkeQjS8Cq1xwktUNZinz26WEapjItoF_iD0AhExR-zH3DHFdE9ynBx4msa6ngY',
+      Authorization: this.token,
     });
 
     return this._http.get('https://api.spotify.com/v1/browse/new-releases', {
       headers,
     });
+  }
+
+  getTracks(term) {
+    const headers = new HttpHeaders({
+      Authorization: this.token,
+    });
+
+    return this._http.get(
+      `https://api.spotify.com/v1/search?q=${term}&type=track&limit=25`,
+      {
+        headers,
+      }
+    );
   }
 }
