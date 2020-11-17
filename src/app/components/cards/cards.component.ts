510d0c1d9b950fa4177c9b2adab78a0a06b5c23c
Uriel Aispuro <urielalexis64@gmail.com>
2020-11-16 21:13:57 -0700

    add cards component

---
+++ src/app/components/cards/cards.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-cards',
+    templateUrl: './cards.component.html',
+    styleUrls: ['./cards.component.css'],
+})
+export class CardsComponent {
+    @Input() items: any[] = [];
+
+    constructor(private _router: Router) {}
+
+    seeArtist(item: any) {
+        let artistID;
+
+        if (item.type === 'artist') {
+            artistID = item.id;
+        } else {
+            artistID = item.artists[0].id;
+        }
+
+        this._router.navigate(['artist/', artistID]);
+    }
+}
