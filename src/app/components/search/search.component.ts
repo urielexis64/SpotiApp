dbae23d198a5f2c1cef7af26f80540173cfeb649
Uriel Aispuro <urielalexis64@gmail.com>
2020-11-16 21:17:05 -0700

    search component optimized

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,19 +2,22 @@ import { Component } from '@angular/core';
 import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styles: [],
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styles: [],
 })
 export class SearchComponent {
-  tracks: any[] = [];
+    artists: any[] = [];
+    loading: boolean = false;
 
-  constructor(private _spotify: SpotifyService) {}
+    constructor(private _spotify: SpotifyService) {}
 
-  search(input: string) {
-    this._spotify.getTracks(input).subscribe((data: any) => {
-      this.tracks = data.tracks.items;
-      console.log(data);
-    });
-  }
+    search(input: string) {
+        this.loading = true;
+        this._spotify.getArtists(input).subscribe((data) => {
+            this.artists = data;
+            this.loading = false;
+            console.log(data);
+        });
+    }
 }
