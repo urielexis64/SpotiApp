520417ad34b6b1fc8ef490e9c04fca22e6789de3
Uriel Aispuro <urielalexis64@gmail.com>
2020-11-17 20:47:04 -0700

    Error message when token expires

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,10 +11,33 @@ export class HomeComponent {
 
     loading: boolean = true;
 
+    error: Error = {
+        message: '',
+        status: 0,
+        ok: true,
+    };
+
     constructor(private spotify: SpotifyService) {
-        spotify.getNewReleases().subscribe((data) => {
-            this.newReleases = data;
-            this.loading = false;
-        });
+        spotify.getNewReleases().subscribe(
+            (data) => {
+                this.newReleases = data;
+                this.loading = false;
+            },
+            (err: any) => {
+                console.log(err);
+                this.error = {
+                    message: err.error.error.message,
+                    status: err.status,
+                    ok: err.ok,
+                };
+                this.loading = false;
+            }
+        );
     }
+}
+
+interface Error {
+    message: string;
+    status: number;
+    ok: boolean;
 }
